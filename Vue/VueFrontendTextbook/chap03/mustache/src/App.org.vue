a2e86117fad12e09ed9a2636333035006d696b78
RKo-aws1 <ryukocndevaws1@gmail.com>
2025-10-29 05:23:02 -0700

    (chap03) App.vue updated.

--- Vue/VueFrontendTextbook/chap03/mustache/src/App.org.vue
+++ Vue/VueFrontendTextbook/chap03/mustache/src/App.org.vue
@@ -1,12 +1,22 @@
 <script setup lang="ts">
-import {ref} from 'vue'
-const name = ref('Scott')
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <h1>
-    Hello, {{ name }}!
-  </h1> 
+  <div id="app">
+    <header>
+      <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+      <div class="wrapper">
+        <HelloWorld msg="You did it!" />
+      </div>
+    </header>
+
+    <main>
+      <TheWelcome />
+    </main>
+  </div>
 </template>
 
 <style scoped>
